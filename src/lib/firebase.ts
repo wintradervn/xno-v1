388b7b0856e9b080f01cf21e0f8eb792becf9e40
deep <deep@unibot.app>
2025-04-02 19:55:08 +0700

    change firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,24 +1,24 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAoujKD36-j-bQU1G6J5eVz0ODfCOYhE4I",
-  authDomain: "xno-dev.firebaseapp.com",
-  projectId: "xno-dev",
-  storageBucket: "xno-dev.firebasestorage.app",
-  messagingSenderId: "696663638281",
-  appId: "1:696663638281:web:47a2d66d650796ad8ad3d8",
-  measurementId: "G-WPCKS8QR63",
-};
-
 // const firebaseConfig = {
-//   apiKey: "AIzaSyCBBOwar3xoFDUfGQYSd3r6zMGvr23h6fQ",
-//   authDomain: "xno-vn-77549.firebaseapp.com",
-//   projectId: "xno-vn-77549",
-//   storageBucket: "xno-vn-77549.firebasestorage.app",
-//   messagingSenderId: "445219967900",
-//   appId: "1:445219967900:web:7f76e8d3f70a8222ccae59",
+//   apiKey: "AIzaSyAoujKD36-j-bQU1G6J5eVz0ODfCOYhE4I",
+//   authDomain: "xno-dev.firebaseapp.com",
+//   projectId: "xno-dev",
+//   storageBucket: "xno-dev.firebasestorage.app",
+//   messagingSenderId: "696663638281",
+//   appId: "1:696663638281:web:47a2d66d650796ad8ad3d8",
+//   measurementId: "G-WPCKS8QR63",
 // };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBBOwar3xoFDUfGQYSd3r6zMGvr23h6fQ",
+  authDomain: "xno-vn-77549.firebaseapp.com",
+  projectId: "xno-vn-77549",
+  storageBucket: "xno-vn-77549.firebasestorage.app",
+  messagingSenderId: "445219967900",
+  appId: "1:445219967900:web:7f76e8d3f70a8222ccae59",
+};
 
 // Initialize Firebase
 const app =
